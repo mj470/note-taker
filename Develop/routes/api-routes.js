ec5c7b7bb3f95521b059829b844250c921f4f0af
Maren Christensen <marenjoychristensen@gmail.com>
2023-08-13 02:34:58 -0500

    Added a Procfile.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,33 +1,45 @@
-const express = require('express'); 
-const path = require('path');
+const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-// we are going to need another file that sends to the screen, a writeFile or sendFile 
+
 router.get('/api/notes', async (req, res) => {
-    const dbJSON = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    res.json(dbJSON);
+    try {
+        const dbJSON = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        res.json(dbJSON);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching notes.' });
+    }
 });
 
 router.post('/api/notes', (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
-    };
-    dbJSON.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(dbJSON));
-    res.json(dbJSON);
+    try {
+        const dbJSON = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuidv4(),
+        };
+        dbJSON.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(dbJSON));
+        res.json(dbJSON);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while saving the note.' });
+    }
 });
 
 router.delete('/api/notes/:id', (req, res) => {
-    let data = fs.readFileSync('./db/db.json', 'utf8');
-    const datadbJSON = JSON.parse(data);
-    const newdbJSON = datadbJSON.filter((note) => note.id !== req.params.id);
-    fs.writeFileSync('./db/db.json', JSON.stringify(newdbJSON));
-res.json(newdbJSON);
+    try {
+        let noteData = fs.readFileSync('./db/db.json', 'utf8');
+        const datadbJSON = JSON.parse(noteData);
+        const newdbJSON = datadbJSON.filter((note) => {
+            return note.id != req.params.id;
+        });
+        fs.writeFileSync('./db/db.json', JSON.stringify(newdbJSON));
+        res.json(newdbJSON);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while deleting the note.' });
+    }
 });
-
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const htmlRouter = require('./routes/html-routes');
 const apiRouter = require('./routes/api-routes');
-const notesRouter = require('./routes/api-routes');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = 3001;
 
+
+app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use(htmlRouter);
 app.use(apiRouter);
-app.use('/', notesRouter);
 
-const server = app.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`Application successfully listening to http://localhost:${PORT}`);
 });
 
-server.on('error', (error) => {
-    console.error('Error starting the server:', error);
-});
